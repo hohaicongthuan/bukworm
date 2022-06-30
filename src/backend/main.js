d53ff03371ebdcbf6b5c11cc021b7e37d2a436b5
Thuan Hai Cong Ho <hohaicongthuan@gmail.com>
2022-06-30 17:28:13 +0700

    Cleaning up codes and Uniforming quotes

--- src/backend/main.js
+++ src/backend/main.js
@@ -1,28 +1,28 @@
-window.addEventListener('pywebviewready', function() {
+window.addEventListener("pywebviewready", function() {
     
     console.log("pywebview is ready!");
 
-    var tools = document.getElementsByClassName('tool-item');
+    var tools = document.getElementsByClassName("tool-item");
     
     // tools[0] is the Open File button
-    tools[0].addEventListener('click', function() {
+    tools[0].addEventListener("click", function() {
         var file_path = pywebview.api.open_file();
-        // console.log('File path is ', file_path);
+        // console.log("File path is ", file_path);
         file_path.then(function(value) {
-            // If user close the file dialog with no file selected, the return value is 'null'
+            // If user close the file dialog with no file selected, the return value is "null"
             if (value != null) {
                 
                 var book_content = pywebview.api.get_content(value[0]); // The API return a promise object
                 console.log(book_content);
-                var read_zone = document.getElementsByClassName('read-zone');
-                var loading = document.getElementsByClassName('loading');
+                var read_zone = document.getElementsByClassName("read-zone");
+                var loading = document.getElementsByClassName("loading");
 
                 loading[0].style.display = "block"; // Show loading notification
 
-                // If the promise object is fulfilled, it'll return the value to the function
+                // If the promise object is fulfilled, it"ll return the value to the function
                 book_content.then(function(value) {
                     
-                    read_zone[0].innerHTML = ''
+                    read_zone[0].innerHTML = ""
 
                     if (Array.isArray(value)) {
                         for (var i = 0; i < value.length; i++) {
@@ -32,7 +32,7 @@ window.addEventListener('pywebviewready', function() {
                         read_zone[0].innerHTML += value;
                     }
 
-                    loading[0].style.display = "none"; // Hide 'loading' notification
+                    loading[0].style.display = "none"; // Hide "loading" notification
                 });
             }
         });
@@ -40,12 +40,12 @@ window.addEventListener('pywebviewready', function() {
     });
 
     // tools[2] is the Exit button
-    tools[2].addEventListener('click', function() {
+    tools[2].addEventListener("click", function() {
         pywebview.api.exit();
     });
 
     // // tools[3] is the About button
-    // tools[3].addEventListener('click', function() {
+    // tools[3].addEventListener("click", function() {
     //     pywebview.api.about();
     // });
 })
