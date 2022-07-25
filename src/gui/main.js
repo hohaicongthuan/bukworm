3cfac4c50bb487067419c152bdde6e4b2dbe8227
Thuan Hai Cong Ho <hohaicongthuan@gmail.com>
2022-07-25 14:53:09 +0700

    Temporarily drop support for PDF
    Temporarily drop support for PDF and MS document files from the active dev branch because I haven't figured out how to improve the performance.

--- src/gui/main.js
+++ src/gui/main.js
@@ -2,40 +2,6 @@ window.addEventListener("pywebviewready", function()
 {
     console.log("pywebview is ready!");
 
-    // Return true if provided element is visible on the viewport
-    function isVisible(el)
-    {
-        let rect = el.getBoundingClientRect();
-        let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-        let viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-        return (
-            rect.top >= -2000 && rect.top <= viewportHeight + 2000
-        );
-    }
-
-    let read_zone = document.getElementsByClassName("read-zone")[0];
-    read_zone.addEventListener("scroll", function()
-    {
-        let pages = document.getElementsByClassName("page");
-        for (let i = 0; i < pages.length; i++)
-        {
-            if (pages[i].getAttribute("data-booktype") == "pdf") {
-                if (isVisible(pages[i]) && pages[i].innerHTML == "")
-                {
-                    let page_img = pywebview.api.load_page(pages[i].getAttribute("data-filepath"), pages[i].getAttribute("id"));
-                    page_img.then(function(value)
-                    {
-                        pages[i].innerHTML = value;
-                    });
-                }
-                // else
-                // {
-                //     pages[i].innerHTML = "";
-                // }
-            } 
-        }
-    });
-
     let tools = document.getElementsByClassName("tool-item");
     
     // tools[0] is the Open File button
--- src/main.py
+++ src/main.py
@@ -5,19 +5,15 @@ import webview
 
 from epubviewer import epubviewer
 from mdviewer import mdviewer
-from msdocviewer import msdocviewer
-from pdfviewer import pdfviewer
 
 
 def open_file(file_path=""):
     if (file_path == ""):
-        types = ("Ebook file (*.epub;*.pdf;*.docx;*.md)", "All files (*.*)")
+        types = ("Ebook file (*.epub;*.md)", "All files (*.*)")
         file_path = window.create_file_dialog(dialog_type=webview.OPEN_DIALOG, directory=".", allow_multiple=False, file_types=types)
         file_path = file_path[0]
         if (file_path == ""):    # User cancelled the open file dialog
             return 0
-
-    # print(f"File path = {path}")
 
     if not (os.path.isfile(file_path)):
         print("File does not exist or is not a file, please check again.")
@@ -32,8 +28,6 @@ def open_file(file_path=""):
     elif (file_ext == ".md"):
         book_content = mdviewer.read(file_path)
         book_content = mdviewer.prep_content(book_content)
-    elif (file_ext == ".pdf"):
-        book_content = pdfviewer.read(file_path)
     else:
         book_content = ""
 
@@ -52,19 +46,6 @@ def clean_read_zone():
     )
 
 
-def load_page(file_path, num):
-    page_img = pdfviewer.load_page(file_path, num)
-    return page_img
-    # print(
-    #     f"let page = document.getElementById('{num}');"
-    #     f'page.innerHTML = "{page_img}";'
-    # )
-    # window.evaluate_js(
-    #     f"let page = document.getElementById('{num}');"
-    #     f'page.innerHTML = "{page_img}";'
-    # )
-
-
 def exit():
     window.destroy()
 
@@ -76,16 +57,6 @@ def main(window):
 
 
 if __name__ == "__main__":
-
     window = webview.create_window("BUKWORM", "gui/index.html")
-    window.expose(open_file, exit, load_page)
+    window.expose(open_file, exit)
     webview.start(main, window, debug=True)
-
-    # for item in book.get_items():
-    #     print("==================================================================================")
-    #     print("ITEM:", item)
-    #     print("----------------------------------------------------------------------------------")
-    #     print("NAME:", os.path.basename(item.get_name()))
-    #     # print("----------------------------------------------------------------------------------")
-    #     # print(item.get_content())
-    #     print("==================================================================================")
